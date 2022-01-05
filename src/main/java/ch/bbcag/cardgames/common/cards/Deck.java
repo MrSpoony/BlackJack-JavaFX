56fe2d89ffbbf7e38b243d253287edc747ff8b2e
Kimi Löffel <kimi.loeffel@hispeed.ch>
2022-01-05 16:01:34 +0100

    Simple Deck with consists of a List of Cards

---
+++ src/main/java/ch/bbcag/cardgames/common/cards/Deck.java
@@ -0,0 +1,9 @@
+package ch.bbcag.cardgames.common.cards;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Deck {
+
+    List<Card> poker = new CopyOnWriteArrayList<>();
+}
--- src/main/java/ch/bbcag/cardgames/common/decks/Deck.java
+++
@@ -1,4 +0,0 @@
-package ch.bbcag.cardgames.common.decks;
-
-public class Deck {
-}
