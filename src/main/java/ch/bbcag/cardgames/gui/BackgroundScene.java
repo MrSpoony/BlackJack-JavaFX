4bb57f0ab9d8693395daaa4d4ef390d08fe78b11
bherma <bherma@bbcag.ch>
2022-01-06 13:12:57 +0100

    New working BackgroundScene

--- src/main/java/ch/bbcag/cardgames/gui/BackgroundScene.java
+++
@@ -1,9 +0,0 @@
-package ch.bbcag.cardgames.gui;
-
-import ch.bbcag.cardgames.common.scene.Navigator;
-
-public class BackgroundScene extends Gui{
-    public BackgroundScene(Navigator navigator) {
-        super(navigator);
-    }
-}
