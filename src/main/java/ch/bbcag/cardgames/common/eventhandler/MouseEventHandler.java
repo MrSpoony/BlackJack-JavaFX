090d708ba00b93542a7c2b094f59eead916b4b85
bherma <bherma@bbcag.ch>
2022-01-06 13:12:22 +0100

    New EventHandler Package with new MouseEventhandler

---
+++ src/main/java/ch/bbcag/cardgames/common/eventhandler/MouseEventHandler.java
@@ -0,0 +1,11 @@
+package ch.bbcag.cardgames.common.eventhandler;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class MouseEventHandler implements EventHandler<MouseEvent> {
+    @Override
+    public void handle(MouseEvent event) {
+
+    }
+}
