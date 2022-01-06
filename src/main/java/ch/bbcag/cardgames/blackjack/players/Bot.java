f3ded0914cc7fbdc34b779e457c213967329d39a
Kimi Löffel <klo132407@stud.gibb.ch>
2022-01-06 12:01:06 +0100

    Added Bot and RealPlayer classes

---
+++ src/main/java/ch/bbcag/cardgames/blackjack/players/Bot.java
@@ -0,0 +1,20 @@
+package ch.bbcag.cardgames.blackjack.players;
+
+import ch.bbcag.cardgames.blackjack.Stack;
+
+public class Bot extends Player{
+
+    public Bot(Stack stack) {
+        super(stack);
+    }
+
+    @Override
+    public void drawNext() {
+
+    }
+
+    @Override
+    public void split() {
+
+    }
+}
---
+++ src/main/java/ch/bbcag/cardgames/blackjack/players/RealPlayer.java
@@ -0,0 +1,19 @@
+package ch.bbcag.cardgames.blackjack.players;
+
+import ch.bbcag.cardgames.blackjack.Stack;
+
+public class RealPlayer extends Player{
+    public RealPlayer(Stack stack) {
+        super(stack);
+    }
+
+    @Override
+    public void drawNext() {
+
+    }
+
+    @Override
+    public void split() {
+
+    }
+}
