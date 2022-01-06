7cf226cf6f516f1bad1ee2e5ea37798ea9240981
bherma <bherma@bbcag.ch>
2022-01-06 20:38:25 +0100

    set the size and the font for the buttons

--- src/main/java/ch/bbcag/cardgames/gui/common/TransparentButton.java
+++ src/main/java/ch/bbcag/cardgames/gui/common/TransparentButton.java
@@ -6,6 +6,7 @@ public class TransparentButton extends Button {
 
     public TransparentButton(String text) {
         super(text);
-        this.setStyle("-fx-background-color:rgba(0.5, 0.5, 0.5, 0.2); -fx-text-fill: #ffffff");
+        this.setStyle("-fx-background-color:rgba(0.5, 0.5, 0.5, 0.2); -fx-text-fill: #ffffff;" +
+                " -fx-font-size: 18; -fx-font-family: Arial;-fx-min-height: 15; -fx-min-width: 20; -fx-start-margin: 6");
     }
 }
