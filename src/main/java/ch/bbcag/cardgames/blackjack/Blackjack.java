55403ea648573a9f73353ddf2d0f73bfe27439fe
Kimi Löffel <klo132407@stud.gibb.ch>
2022-01-06 12:00:14 +0100

    Added main Blackjack class

---
+++ src/main/java/ch/bbcag/cardgames/blackjack/Blackjack.java
@@ -0,0 +1,37 @@
+package ch.bbcag.cardgames.blackjack;
+
+import ch.bbcag.cardgames.blackjack.players.Bot;
+import ch.bbcag.cardgames.blackjack.players.Player;
+import ch.bbcag.cardgames.blackjack.players.RealPlayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Blackjack {
+
+    public static final int VALUE_TO_WIN = 21;
+    private static final int NUMBER_OF_DECKS_USED = 6;
+    private static final int NUMBER_OF_PLAYERS = 6;
+
+    private int numberOfRealPlayers;
+    private int numberOfBots;
+    private Stack mainStack = new Stack(NUMBER_OF_DECKS_USED);
+
+    private List<Player> players = new ArrayList<>();
+
+    public Blackjack(int numberOrRealPlayers) {
+        this.numberOfRealPlayers = numberOrRealPlayers;
+        numberOfBots = NUMBER_OF_PLAYERS - (numberOrRealPlayers + 1);
+        setupPlayers();
+    }
+
+    private void setupPlayers() {
+        players.add(new Dealer(mainStack));
+        for (int i = 0; i < numberOfRealPlayers; i++) {
+            players.add(new RealPlayer(mainStack));
+        }
+        for (int i = 0; i < numberOfBots; i++) {
+            players.add(new Bot(mainStack));
+        }
+    }
+}
