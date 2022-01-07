7a7f813e267927ea6422686239291923702bca6d
bherma <bherma@bbcag.ch>
2022-01-07 15:46:02 +0100

    I started to implement the backend.

--- src/main/java/ch/bbcag/cardgames/gui/scenes/BlackjackScene.java
+++ src/main/java/ch/bbcag/cardgames/gui/scenes/BlackjackScene.java
@@ -2,23 +2,38 @@ package ch.bbcag.cardgames.gui.scenes;
 
 
 import ch.bbcag.cardgames.blackjack.Blackjack;
+import ch.bbcag.cardgames.blackjack.Stack;
 import ch.bbcag.cardgames.blackjack.buttonhandler.DoubleButtonHandler;
 import ch.bbcag.cardgames.blackjack.buttonhandler.HitButtonHandler;
 import ch.bbcag.cardgames.blackjack.buttonhandler.HoldButtonHandler;
 import ch.bbcag.cardgames.blackjack.buttonhandler.SplitButtonHandler;
+import ch.bbcag.cardgames.blackjack.players.Player;
 import ch.bbcag.cardgames.common.cards.Card;
 import ch.bbcag.cardgames.common.scene.BaseScene;
 import ch.bbcag.cardgames.common.scene.Navigator;
 import ch.bbcag.cardgames.gui.common.*;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 
 public class BlackjackScene extends BackgroundScene {
 
     private static int NUMBER_OF_PLAYERS = 1;
+    private final int NUMBER_OF_DECKS = 6;
+    private String cardFace;
 
-    private Blackjack blackjack = new Blackjack(NUMBER_OF_PLAYERS);
+    private Blackjack blackjack;
+    private Stack stack = new Stack(NUMBER_OF_DECKS);
+    private Player player;
+
+    private List<Card> cards = new ArrayList<>();
 
 
     private final TransparentButton btnSplit = new TransparentButton("Split");
@@ -28,14 +43,16 @@ public class BlackjackScene extends BackgroundScene {
     private final TransparentButton btnHelp = new TransparentButton("Help");
     private final TransparentButton btnSet = new TransparentButton("Set");
 
-    private final SplitButtonHandler splitButtonHandler = new SplitButtonHandler(blackjack.getPlayer());
-    private final DoubleButtonHandler doubleButtonHandler = new DoubleButtonHandler(blackjack.getPlayer());
-    private final HitButtonHandler hitButtonHandler = new HitButtonHandler(blackjack.getPlayer());
-    private final HoldButtonHandler holdButtonHandler = new HoldButtonHandler(blackjack.getPlayer());
+    private SplitButtonHandler splitButtonHandler;
+    private DoubleButtonHandler doubleButtonHandler;
+    private HitButtonHandler hitButtonHandler;
+    private HoldButtonHandler holdButtonHandler;
 
     private final LabelLayout moneyLbl = new LabelLayout("Money:");
     private final LabelLayout subtotalLbl = new LabelLayout("Subtotal:");
     private final LabelLayout moneyInsertLbl = new LabelLayout("Your Insert:");
+
+    private final String IMAGE_PATH = "/pokerdeck/2B.png";
 
     private final TextField moneyInsertTxt = new TextField("Enter your Money Insert");
 
@@ -46,12 +63,13 @@ public class BlackjackScene extends BackgroundScene {
     private final double textFieldWidth = 150;
     private final double textFieldHeight = 30;
 
-    private Card card1 = new Card("2C.png");
-    private Card card2 = new Card("3C.png");
-    private Card card3 = new Card("4C.png");
-    private Card card4 = new Card("5C.png");
-    private Card card5 = new Card("2B.png");
+    private double posX = 150;
+    private final double posY = 360;
+    private final double widtForCards = 125;
+    private final double heightForCards = 175;
 
+    //private Card card = new Card(cardFace);
+    private Image card5 = new Image(Objects.requireNonNull(this.getClass().getResourceAsStream(IMAGE_PATH)));
 
     public BlackjackScene(Navigator navigator) {
         super(navigator);
@@ -63,6 +81,11 @@ public class BlackjackScene extends BackgroundScene {
         btnSplit.setOnMouseClicked(splitButtonHandler);
         btnHit.setOnMouseClicked(hitButtonHandler);
         btnHold.setOnMouseClicked(holdButtonHandler);
+
+        player = blackjack.getPlayer();
+        cards = player.getCards();
+
+
     }
 
     @Override
@@ -89,15 +112,15 @@ public class BlackjackScene extends BackgroundScene {
 
         mainHbox.setSpacing(5);
 
-        gc.drawImage(card1.getImage(), 150, 360, 125, 175);
-        gc.drawImage(card2.getImage(), 250, 360, 125, 175);
-        gc.drawImage(card3.getImage(), 350, 360, 125, 175);
-        gc.drawImage(card4.getImage(), 450, 360, 125, 175);
-
-        gc.drawImage(card5.getImage(), 150, 10, 62, 87);
-        gc.drawImage(card2.getImage(), 210, 10, 62, 87);
-        gc.drawImage(card3.getImage(), 260, 10, 62, 87);
-        gc.drawImage(card4.getImage(), 310, 10, 62, 87);
+        //drawcards(gc, cards);
+//        gc.drawImage(card1.getImage(), 150, 360, 125, 175);
+//        gc.drawImage(card2.getImage(), 250, 360, 125, 175);
+//        gc.drawImage(card3.getImage(), 350, 360, 125, 175);
+//        gc.drawImage(card4.getImage(), 450, 360, 125, 175);
+//
+//        gc.drawImage(card5, 310, 10, 62, 87);
+//        gc.drawImage(card3.getImage(), 410, 10, 62, 87);
+//        gc.drawImage(card4.getImage(), 460, 10, 62, 87);
 
         mainAnchorPain.getChildren().addAll(moneyLbl, subtotalLbl, moneyInsertTxt, moneyInsertLbl, mainHbox, hBoxForHelpbtn, hBoxForSetbtn);
 
@@ -109,4 +132,32 @@ public class BlackjackScene extends BackgroundScene {
         getGroup().getChildren().add(mainAnchorPain);
     }
 
+    @Override
+    public void onEnter() {
+        blackjack = new Blackjack(NUMBER_OF_PLAYERS);
+
+        splitButtonHandler = new SplitButtonHandler(blackjack.getPlayer());
+        doubleButtonHandler = new DoubleButtonHandler(blackjack.getPlayer());
+        hitButtonHandler = new HitButtonHandler(blackjack.getPlayer());
+        holdButtonHandler = new HoldButtonHandler(blackjack.getPlayer());
+
+        super.onEnter();
+    }
+
+/*
+    private void drawcards(GraphicsContext gc, List<Card> cards) {
+
+        for (Card card : cards) {
+            if (btnHit.isPressed()) {
+                 gc.drawImage(card.getImage(), posX, posY, widtForCards, heightForCards);
+                 posX += 50;
+                 System.out.println(posX);
+            }
+            else if (!btnHit.isPressed()){
+                break;
+            }
+        }
+    }
+
+ */
 }
