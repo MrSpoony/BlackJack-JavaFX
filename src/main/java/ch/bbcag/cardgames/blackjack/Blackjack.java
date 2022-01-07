be8b7b8909856f39af30dab8de1c5406e87cc765
btrums <btrums@bbcag.ch>
2022-01-07 11:24:01 +0100

    Blackjack finished except returning winner to Front-End.

--- src/main/java/ch/bbcag/cardgames/blackjack/Blackjack.java
+++ src/main/java/ch/bbcag/cardgames/blackjack/Blackjack.java
@@ -12,11 +12,15 @@ public class Blackjack {
 
     public static final int VALUE_TO_WIN = 21;
     public static final int NUMBER_OF_CARDS_TO_GET_AT_BEGIN = 2;
-    private static final int NUMBER_OF_DECKS_USED = 6;
+    private static final int NUMBER_OF_DECKS_USED = 10;
     private static final int NUMBER_OF_PLAYERS = 6;
 
     private int numberOfRealPlayers;
     private int numberOfBots;
+    private int dealerHand;
+    private int playerHand;
+    private Player dealer;
+    private Player winner;
     private Stack mainStack = new Stack(NUMBER_OF_DECKS_USED);
 
     private List<Player> players = new ArrayList<>();
@@ -24,7 +28,67 @@ public class Blackjack {
     public Blackjack(int numberOrRealPlayers) {
         this.numberOfRealPlayers = numberOrRealPlayers;
         numberOfBots = NUMBER_OF_PLAYERS - (numberOrRealPlayers + 1);
+        setupGame();
+        startTurns();
+        if(!isDraw()){
+            winner = findWinner(); // Winner is the Player that won.
+        }
+        // else Its a draw ;
+    }
+
+    public RealPlayer getCurrentRealPlayer() {
+        for (Player player : players) {
+            if (player instanceof RealPlayer) {
+                return (RealPlayer) player;
+            }
+        }
+        throw new IllegalStateException("There is no real player");
+    }
+    public void startTurns(){
+        if(!players.get(players.size() - 1).isDone())
+        for(Player player : players){
+            player.turn();
+        }
+
+    }
+    private Player findWinner(){
+        if(playerHand < VALUE_TO_WIN + 1){
+            if(dealerHand < VALUE_TO_WIN + 1){
+                if(dealerHand < playerHand){
+                    return getCurrentRealPlayer();
+                }
+                else if(dealerHand > playerHand){
+                    return dealer;
+                }
+            }
+            else return getCurrentRealPlayer();
+        }
+        else return dealer;
+        return dealer;
+    }
+    private boolean isAmountTheSame(){
+        return players.get(players.size() - 1).getCount(Count.BEST) == getCurrentRealPlayer().getCount(Count.BEST);
+    }
+    private boolean isDraw(){
+        return isAmountTheSame() && getCurrentRealPlayer().getCount(Count.BEST) < VALUE_TO_WIN + 1;
+    }
+    private void setupVariables(){
+        dealer = players.get(players.size() - 1);
+        dealerHand = dealer.getCount(Count.BEST);
+        playerHand = getCurrentRealPlayer().getCount(Count.BEST);
+    }
+    private void dealStartCards(){
+        for(int i = 0; i < NUMBER_OF_CARDS_TO_GET_AT_BEGIN; i++) {
+            for (Player player : players) {
+                player.takeCard();
+            }
+        }
+    }
+
+    private void setupGame(){
         setupPlayers();
+        setupVariables();
+        dealStartCards();
     }
 
     private void setupPlayers() {
@@ -35,14 +99,5 @@ public class Blackjack {
             players.add(new Bot(mainStack));
         }
         players.add(new Dealer(mainStack));
-    }
-
-    public RealPlayer getCurrentRealPlayer() {
-        for (Player player : players) {
-            if (player instanceof RealPlayer) {
-                return (RealPlayer) player;
-            }
-        }
-        throw new IllegalStateException("There is no real player");
     }
 }
