9e5e3cd069ae799188760305b1bf1a204a08d65d
Kimi Löffel <klo132407@stud.gibb.ch>
2022-01-12 13:38:07 +0100

    Cleaned up BlackjackScene.

--- src/main/java/ch/bbcag/cardgames/gui/scenes/BlackjackScene.java
+++ src/main/java/ch/bbcag/cardgames/gui/scenes/BlackjackScene.java
@@ -40,7 +40,7 @@ public class BlackjackScene extends BackgroundScene {
     private static final double HEIGHT_DEALER_CARDS = 117;
     private static final double POSITION_Y_DEALER_CARDS = CARDS_Y_OFFSET;
     private static final double DEALER_CARDS_INITIAL_X = 300;
-    private static final double DEALER_CARDS_X_SPACE = canvas.getWidth() - (2 *DEALER_CARDS_INITIAL_X);
+    private static final double DEALER_CARDS_X_SPACE = canvas.getWidth() - (2 * DEALER_CARDS_INITIAL_X);
     private static final double DEALER_CARDS_INCREMENT_CHANGE = 3;
 
 
@@ -57,13 +57,17 @@ public class BlackjackScene extends BackgroundScene {
     private static final LabelLayout INSERT_MONEY_LABEL = new LabelLayout("Your Insert:");
     private static final TextField INSERT_MONEY_TEXT_FIELD = new TextField("Enter your Money ");
 
+    private static final double MONEY_TEXT_FIELD_WIDTH = 150;
+    private static final double MONEY_TEXT_FIELD_HEIGHT = 30;
 
     private static final double MARGIN_ANCHOR_PANE = 10.0;
     private static final double MARGIN_MONEY_INSERTS = 75.5;
     private static final double MARGIN_BUTTONS = 50.0;
 
+    private static final double SPACING_IN_H_BOXES = 5;
+
     private static final AnchorPane ANCHOR_PANE = new AnchorPane();
-    private static final HBox H_BOX = new HBox();
+    private static final HBox BOTTOM_RIGHT_H_BOX = new HBox();
     private static final HBox TOP_RIGHT_H_BOX = new HBox();
     private static final HBox H_BOX_SET_BUTTON = new HBox();
 
@@ -73,9 +77,9 @@ public class BlackjackScene extends BackgroundScene {
     private List<Card> playerCards = new ArrayList<>();
     private List<Card> dealerCards = new ArrayList<>();
 
-
-
-
+    private Blackjack blackjack;
+    private RealPlayer player;
+    private Dealer dealer;
 
     public BlackjackScene(Navigator navigator) {
         super(navigator);
@@ -88,12 +92,74 @@ public class BlackjackScene extends BackgroundScene {
 
     @Override
     public void paint() {
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        clearCanvas();
         super.paint();
-
-
         drawPlayerCards();
         drawDealerCards();
+    }
+
+    @Override
+    public void onEnter() {
+        super.onEnter();
+        setupVariables();
+        setupButtonHandlers();
+        setPositionOfNodes();
+
+        H_BOX_SET_BUTTON.getChildren().add(SET_BUTTON);
+
+        INSERT_MONEY_TEXT_FIELD.setPrefSize(MONEY_TEXT_FIELD_WIDTH, MONEY_TEXT_FIELD_HEIGHT);
+
+        setupTopRightHBox();
+        setupBottomRightHBox();
+        setupAnchorpane();
+    }
+
+    private void setupAnchorpane() {
+        ANCHOR_PANE.setPrefSize(BaseScene.SCREEN_WIDTH, BaseScene.SCREEN_HEIGHT);
+        ANCHOR_PANE.getChildren().addAll(MONEY_LABEL, SUBTOTAL_LABEL, INSERT_MONEY_TEXT_FIELD, INSERT_MONEY_LABEL, BOTTOM_RIGHT_H_BOX, TOP_RIGHT_H_BOX, H_BOX_SET_BUTTON);
+        getGroup().getChildren().add(ANCHOR_PANE);
+    }
+
+    private void setupBottomRightHBox() {
+        BOTTOM_RIGHT_H_BOX.setSpacing(SPACING_IN_H_BOXES);
+        BOTTOM_RIGHT_H_BOX.getChildren().addAll(HIT_BUTTON, HOLD_BUTTON, DOUBLE_BUTTON, SPLIT_BUTTON);
+    }
+
+    private void setupTopRightHBox() {
+        TOP_RIGHT_H_BOX.setSpacing(SPACING_IN_H_BOXES);
+        TOP_RIGHT_H_BOX.getChildren().addAll(HELP_BUTTON, EXIT_BUTTON);
+    }
+
+    private void setPositionOfNodes() {
+        PositionOfNodes.setAllFourPositions(MONEY_LABEL, HELP_BUTTON, INSERT_MONEY_LABEL, SUBTOTAL_LABEL, MARGIN_ANCHOR_PANE);
+        PositionOfNodes.setBottomRightForSpecials(INSERT_MONEY_LABEL, 100.0, MARGIN_MONEY_INSERTS);
+        PositionOfNodes.setBottomRightForSpecials(INSERT_MONEY_TEXT_FIELD, MARGIN_MONEY_INSERTS, MARGIN_BUTTONS);
+        PositionOfNodes.setBottomRightLbl(BOTTOM_RIGHT_H_BOX, MARGIN_ANCHOR_PANE);
+        PositionOfNodes.setTopRightLbl(TOP_RIGHT_H_BOX, MARGIN_ANCHOR_PANE);
+        PositionOfNodes.setBottomRightForSpecials(H_BOX_SET_BUTTON, MARGIN_ANCHOR_PANE, MARGIN_BUTTONS);
+    }
+
+    private void setupVariables() {
+        blackjack = new Blackjack(NUMBER_OF_PLAYERS);
+        player = blackjack.getPlayer();
+        dealer = blackjack.getDealer();
+        playerCards = player.getCards();
+        dealerCards = dealer.getCards();
+    }
+
+    private void setupButtonHandlers() {
+        SplitButtonHandler splitButtonHandler = new SplitButtonHandler(player);
+        DoubleButtonHandler doubleButtonHandler = new DoubleButtonHandler(player);
+        HitButtonHandler hitButtonHandler = new HitButtonHandler(player);
+        HoldButtonHandler holdButtonHandler = new HoldButtonHandler(player);
+        DOUBLE_BUTTON.setOnAction(doubleButtonHandler);
+        SPLIT_BUTTON.setOnAction(splitButtonHandler);
+        HOLD_BUTTON.setOnAction(holdButtonHandler);
+        HIT_BUTTON.setOnAction(hitButtonHandler);
+    }
+
+    private void clearCanvas() {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
     private void drawPlayerCards() {
@@ -108,7 +174,7 @@ public class BlackjackScene extends BackgroundScene {
     private void drawDealerCards() {
         double posXDealerCards = DEALER_CARDS_INITIAL_X;
         calculateDealerXIncrement();
-        for (Card card : dealerCards){
+        for (Card card : dealerCards) {
             gc.drawImage(card.getImage(), posXDealerCards, POSITION_Y_DEALER_CARDS, WIDTH_DEALER_CARDS, HEIGHT_DEALER_CARDS);
             posXDealerCards += dealerXIncrement;
         }
@@ -128,47 +194,5 @@ public class BlackjackScene extends BackgroundScene {
                 dealerXIncrement -= DEALER_CARDS_INCREMENT_CHANGE;
             }
         }
-    }
-
-    @Override
-    public void onEnter() {
-        super.onEnter();
-        Blackjack blackjack = new Blackjack(NUMBER_OF_PLAYERS);
-
-        RealPlayer player = blackjack.getPlayer();
-        Dealer dealer = blackjack.getDealer();
-
-        playerCards = player.getCards();
-        dealerCards = dealer.getCards();
-        ANCHOR_PANE.setPrefSize(BaseScene.SCREEN_WIDTH, BaseScene.SCREEN_HEIGHT);
-
-        SplitButtonHandler splitButtonHandler = new SplitButtonHandler(player);
-        DoubleButtonHandler doubleButtonHandler = new DoubleButtonHandler(player);
-        HitButtonHandler hitButtonHandler = new HitButtonHandler(player);
-        HoldButtonHandler holdButtonHandler = new HoldButtonHandler(player);
-
-        DOUBLE_BUTTON.setOnAction(doubleButtonHandler);
-        SPLIT_BUTTON.setOnAction(splitButtonHandler);
-        HOLD_BUTTON.setOnAction(holdButtonHandler);
-        HIT_BUTTON.setOnAction(hitButtonHandler);
-
-        PositionOfNodes.setAllFourPositions(MONEY_LABEL, HELP_BUTTON, INSERT_MONEY_LABEL, SUBTOTAL_LABEL, MARGIN_ANCHOR_PANE);
-        PositionOfNodes.setBottomRightForSpecials(INSERT_MONEY_LABEL, 100.0, MARGIN_MONEY_INSERTS);
-        PositionOfNodes.setBottomRightForSpecials(INSERT_MONEY_TEXT_FIELD, MARGIN_MONEY_INSERTS, MARGIN_BUTTONS);
-        PositionOfNodes.setBottomRightLbl(H_BOX, MARGIN_ANCHOR_PANE);
-        PositionOfNodes.setTopRightLbl(TOP_RIGHT_H_BOX, MARGIN_ANCHOR_PANE);
-        PositionOfNodes.setBottomRightForSpecials(H_BOX_SET_BUTTON, MARGIN_ANCHOR_PANE, MARGIN_BUTTONS);
-        H_BOX.setSpacing(5);
-        TOP_RIGHT_H_BOX.setSpacing(5);
-
-        double textFieldWidth = 150;
-        double textFieldHeight = 30;
-        INSERT_MONEY_TEXT_FIELD.setPrefSize(textFieldWidth, textFieldHeight);
-
-        H_BOX.getChildren().addAll(HIT_BUTTON, HOLD_BUTTON, DOUBLE_BUTTON, SPLIT_BUTTON);
-        TOP_RIGHT_H_BOX.getChildren().addAll(HELP_BUTTON, EXIT_BUTTON);
-        H_BOX_SET_BUTTON.getChildren().add(SET_BUTTON);
-        ANCHOR_PANE.getChildren().addAll(MONEY_LABEL, SUBTOTAL_LABEL, INSERT_MONEY_TEXT_FIELD, INSERT_MONEY_LABEL, H_BOX, TOP_RIGHT_H_BOX, H_BOX_SET_BUTTON);
-        getGroup().getChildren().add(ANCHOR_PANE);
     }
 }
