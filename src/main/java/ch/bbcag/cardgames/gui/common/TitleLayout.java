e2600103a25de44df19924d7836613407be743e3
bherma <bherma@bbcag.ch>
2022-01-13 16:15:40 +0100

    The Start Scene is ready. Buttons currently doesn't work.

--- src/main/java/ch/bbcag/cardgames/gui/common/TitleLayout.java
+++ src/main/java/ch/bbcag/cardgames/gui/common/TitleLayout.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Label;
 public class TitleLayout extends Label {
     public TitleLayout(String text){
         super(text);
-        this.setStyle("-fx-text-fill: #000000; -fx-font-size: 46; -fx-font-family: 'Perpetua Titling MT'; ");
+        this.setStyle("-fx-text-fill: #000000; -fx-font-size: 100; -fx-font-family: 'Perpetua Titling MT'; ");
     }
-
-
 }
